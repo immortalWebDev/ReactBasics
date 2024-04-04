46193bb38d30cd8962ffc540e67918668909b5b0
immortalWebDev <pgbadgujar007@gmail.com>
2024-04-04 16:20:42 +0530

    Styling components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,16 @@
-function ExpenseItem(){
+import "./ExpenseItem.css";
 
-    return(
-  
-      <>
-        <div>15 August 2023</div>
-        <div>Delhi</div>
-  
-        <div>
-          <h2>Book</h2>
-          <div>$10</div>
-        </div>
-        
-      
-      </>
-    )
-  }
-  export default ExpenseItem
+function ExpenseItem() {
+  return (
+    <div className="expense-item">
+      <div>15 August 2023</div>
+      <div className="expense-item__location">Delhi</div>
+      <div className="expense-item__description">
+        <h2>Book</h2>
+        <div className="expense-item__price">$10</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
