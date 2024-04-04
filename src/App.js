5bb0b410f09bc0f9857f080e0cf75928913a826f
immortalWebDev <pgbadgujar007@gmail.com>
2024-04-04 15:33:58 +0530

    Writing complete JSX

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
   return (
     <div>
-
-      <h1>Let's get started</h1>
-      
+      <h1>Let's get Started</h1>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
