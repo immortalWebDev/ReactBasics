0104db0dcd4a460848be083f7cc67a82dbce83a1
immortalWebDev <pgbadgujar007@gmail.com>
2024-04-07 16:08:30 +0530

    Better design pattern - Conditional return statements- Expenses.js

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,60 @@
-// Write your code at relevant places in the code below:
+// // Write your code at relevant places in the code below:
+
+// import React, { useState } from "react";
+// import ExpenseItem from "./ExpenseItem";
+// import ExpensesFilter from "./ExpensesFilter";
+// import "./Expenses.css";
+// import Card from "../UI/Card";
+
+// const Expenses = (props) => {
+//   const [filteredYear, setFilteredYear] = useState("2023");
+
+//   const changeFilterHandler = (selectedYear) => {
+//     setFilteredYear(selectedYear);
+//   };
+
+//   const filteredExpenses = props.expenses.filter((expense) => {
+//     return expense.date.getFullYear().toString() === filteredYear;
+//   });
+
+//   let expensesContent = <p>No expenses found</p>;
+
+//   if (filteredExpenses.length > 0) {
+//     expensesContent = filteredExpenses.map((expense) => {
+//       return (
+//         <>
+//           <ExpenseItem
+//             key={expense.id}
+//             title={expense.title}
+//             date={expense.date}
+//             price={expense.price}
+//           />
+//           {filteredExpenses.length === 1 && (
+//           <p>Only one expense found. Please add more</p>
+//           )}
+//         </>
+//       );
+//     });
+//   }
+
+//   return (
+//     <Card className="expenses">
+//       <ExpensesFilter
+//         selected={filteredYear}
+//         onChangeFilter={changeFilterHandler}
+//       />
+//       {expensesContent}
+//     </Card>
+//   );
+// };
+
+// export default Expenses;
+
+
 
 import React, { useState } from "react";
-import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 import "./Expenses.css";
 import Card from "../UI/Card";
 
@@ -17,33 +69,13 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  let expensesContent = <p>No expenses found</p>;
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => {
-      return (
-        <>
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            date={expense.date}
-            price={expense.price}
-          />
-          {filteredExpenses.length === 1 && (
-          <p>Only one expense found. Please add more</p>
-          )}
-        </>
-      );
-    });
-  }
-
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={changeFilterHandler}
       />
-      {expensesContent}
+      <ExpensesList items={filteredExpenses} />
     </Card>
   );
 };
