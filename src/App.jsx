7ed4b1936b5f5855365498503ce383f55eb01d28
immortalWebDev <pgbadgujar007@gmail.com>
2024-04-18 00:35:52 +0530

    Counter Project - used useState hook

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import React,{useState} from 'react';
+
+import './App.css'
+
+const App = () => {
+
+  const [counter, setCounter] = useState(0);
+  
+  const addValue = () => {
+    
+    if(counter > 20)
+    return
+    else
+    {
+      setCounter(counter + 1)
+      
+    }
+  
+  };
+ 
+  const removeValue = () => {
+
+    if(counter < 0)
+    return
+    else 
+    {
+      setCounter(counter - 1)
+    }
+
+  }
+
+  return (
+    <>
+      <h1>Counter Project</h1>
+      <h2>Counter: {counter}</h2>
+      <button onClick={addValue}>Add</button>
+      <br></br><br></br>
+      <button onClick={removeValue}>Remove</button>
+    </>
+  );
+};
+
+export default App;
