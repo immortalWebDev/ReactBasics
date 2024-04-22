6a3936b46161a1e26d9e3dbfbbc3ec480ab928d3
immortalWebDev <pgbadgujar007@gmail.com>
2024-04-23 00:14:44 +0530

    Made Add button look good

--- src/components/Meals/MealItem/MealItemForm.jsx
+++ src/components/Meals/MealItem/MealItemForm.jsx
@@ -1,17 +1,26 @@
-import classes from './MealItemForm.module.css'
+import classes from "./MealItemForm.module.css";
+import Input from "../../UI/Input";
 
-const MealItemForm = () => {
+const MealItemForm = (props) => {
+  return (
+    <>
+      <form className={classes.form}>
+        {/* <input type='text' placeholder='Amount'></input> */}
+        <Input
+          label="Amount"
+          input={{
+            id: "amount_" + props.id,
+            type: "number",
+            min: "1",
+            max: "10",
+            step: "1",
+            defaultValue: "1",
+          }}
+        />
+        <button>Add</button>
+      </form>
+    </>
+  );
+};
 
-    return(
-        <>
-        <form className={classes.form}>
-            <input type='text' placeholder='Amount'></input>
-            <button>Add</button>
-        </form>
-        </>
-    )
-
-}
-
-
-export default MealItemForm 
+export default MealItemForm;
