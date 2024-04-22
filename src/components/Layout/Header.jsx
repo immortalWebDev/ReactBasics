4deb6db0499278e3260088974e18e98c73fb7f27
immortalWebDev <pgbadgujar007@gmail.com>
2024-04-22 18:23:12 +0530

    Added styling to cart/button

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -2,13 +2,15 @@ import React from "react";
 
 import mealsImage from "../../assets/meals.jpg";
 import classes from "./Header.module.css";
+import HeaderCartButton from './HeaderCartButton';
 
 const Header = (props) => {
   return (
     <>
       <header className={classes.header}>
         <h1>React Meals</h1>
-        <button>Cart: 0</button>
+        {/* <button>Cart: 0</button> */}
+        <HeaderCartButton></HeaderCartButton>
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="Meals image"></img>
