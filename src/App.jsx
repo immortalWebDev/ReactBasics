9689089c60a312a3355b749166b6aa867c61f3d9
immortalWebDev <pgbadgujar007@gmail.com>
2024-04-22 18:46:01 +0530

    Added Meals card/summary

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,16 @@ import React from "react";
 
 import Header from "./components/Layout/Header";
 
+import Meals from "./components/Meals/Meals";
+
 const App = () => {
 
   return(
     <>
     <Header></Header>
+    <main>
+      <Meals></Meals>
+    </main>
     </>
   )
 }
