e23a573db781a1c42300fb469882b0f9a57e6e93
immortalWebDev <pgbadgujar007@gmail.com>
2024-05-11 13:48:28 +0530

    Added auto retry every 5 sec

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
-import React, { useState } from 'react';
-import MoviesList from './components/MoviesList';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const [isLoading,setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [retryInterval, setRetryInterval] = useState(null);
 
-  //Converted to async await with try & catch blocks
+  
   async function fetchMoviesHandler() {
     try {
-      setIsLoading(true)
-      const response = await fetch('https://swapi.dev/api/films/');
+      setIsLoading(true);
+      setError(null);
+      const response = await fetch("https://swapi.dev/api/films/");
+
+      //Error handling
+      if (!response.ok) {
+        throw new Error("Something went wrong, Retrying...");
+      }
       const data = await response.json();
 
       const transformedMovies = data.results.map((movieData) => {
@@ -23,11 +31,33 @@ function App() {
       });
 
       setMovies(transformedMovies);
-      setIsLoading(false)
+      setIsLoading(false);
+
     } catch (error) {
-      console.error('Error fetching movies:', error);
+      setError(error.message);
+      setIsLoading(false);
+      retryFetchMovies();
     }
   }
+
+  // Function to retry fetching movies after an interval
+  function retryFetchMovies() {
+    setRetryInterval(setInterval(fetchMoviesHandler, 5000));
+  }
+
+  // Function to cancel retrying
+  function cancelRetry() {
+    clearInterval(retryInterval);
+    setIsLoading(false);
+    setError(null);
+  }
+
+  // Cleanup function to clear retry interval on component unmount
+  useEffect(() => {
+    return () => {
+      clearInterval(retryInterval);
+    };
+  }, [retryInterval]);
 
   return (
     <React.Fragment>
@@ -36,8 +66,16 @@ function App() {
       </section>
       <section>
         {!isLoading && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p>No movies found. Fetch to check again.</p>}
+        {!isLoading && movies.length === 0 && !error && (
+          <p>No movies found. Fetch to check again.</p>
+        )}
         {isLoading && <p>Loading...</p>}
+        {error && (
+          <div>
+            <p>{error}</p>
+            <button onClick={cancelRetry}>Cancel</button>
+          </div>
+        )}
       </section>
     </React.Fragment>
   );
