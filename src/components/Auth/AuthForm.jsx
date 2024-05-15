90433e7c0f16f0b81499bfbdaba5fdd9c2632c9b
immortalWebDev <pgbadgujar007@gmail.com>
2024-05-15 11:20:50 +0530

    Added change password logic

--- src/components/Auth/AuthForm.jsx
+++ src/components/Auth/AuthForm.jsx
@@ -1,9 +1,11 @@
 import { useState, useRef ,useContext} from "react";
+import { useHistory } from 'react-router-dom'
 
 import classes from "./AuthForm.module.css";
 import AuthContext from "../../store/auth-context";
 
 const AuthForm = () => {
+  const history = useHistory()
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
 
@@ -56,8 +58,9 @@ const AuthForm = () => {
         }
       })
       .then((data) => {
-        console.log(data);
+        // console.log(data);
         authCtx.login(data.idToken)
+        history.replace('/')
       })
       .catch((err) => {
         alert(err.message);
--- src/components/Profile/ProfileForm.jsx
+++ src/components/Profile/ProfileForm.jsx
@@ -1,17 +1,47 @@
-import classes from './ProfileForm.module.css';
+import React, { useRef, useContext } from "react";
+import { useHistory } from "react-router-dom";
+import classes from "./ProfileForm.module.css";
+import AuthContext from "../../store/auth-context";
 
 const ProfileForm = () => {
+  const history = useHistory()
+  const newPasswordInputRef = useRef();
+  const authCtx = useContext(AuthContext);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredNewPassword = newPasswordInputRef.current.value;
+
+    fetch(
+      `https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyAW9WTtlwepGK4ckucebGnvvBM6cMe1wa0`,
+      {
+        method: "POST",
+        body: JSON.stringify({
+          idToken: authCtx.token,
+          password: enteredNewPassword,
+          returnSecureToken: true,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(response => {
+      history.replace('/')
+    })
+  };
+
   return (
-    <form className={classes.form}>
+    <form onSubmit={submitHandler} className={classes.form}>
       <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <label htmlFor="new-password">New Password</label>
+        <input type="password" id="new-password" minLength='8' ref={newPasswordInputRef} />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
       </div>
     </form>
   );
-}
+};
 
 export default ProfileForm;
